e3f18571352a9db82db049cd84d7ed8beec1fb60
gamyeng <133553103+gamyeng@users.noreply.github.com>
2023-05-14 19:19:50 +0800

    Rename footer.js to footer1.js

--- footer.js
+++
@@ -1,1 +0,0 @@
-document.write('<ul><li><a href="https://gamyeng.github.io/">回到首页</a></li><li><a href="about.html">关于我们</a></li></ul><div class="copyright">© wobo</div>');
---
+++ footer1.js
@@ -0,0 +1,1 @@
+document.write('<ul><li><a href="https://gamyeng.github.io/">回到首页</a></li><li><a href="about.html">关于我们</a></li></ul><div class="copyright">© wobo</div>');
