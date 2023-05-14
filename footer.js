ada64382b6395fc840c5007eb6be8b3921a1eb97
gamyeng <133553103+gamyeng@users.noreply.github.com>
2023-05-14 19:10:58 +0800

    Add files via upload

--- footer.js
+++ footer.js
@@ -1,1 +1,1 @@
-document.write('<ul><li><a href="http://m.66360.cn">回到首页</a></li><li><a href="about.html">关于我们</a></li></ul><div class="copyright">© Bettersoft.cn</div>');
+document.write('<ul><li><a href="https://gamyeng.github.io/">回到首页</a></li><li><a href="about.html">关于我们</a></li></ul><div class="copyright">© wobo</div>');
