b23f8592c2c3a3e85a0af01071cabfdd14753211
gamyeng <133553103+gamyeng@users.noreply.github.com>
2023-05-17 21:30:35 +0800

    Update search.js

--- search.js
+++ search.js
@@ -1,9 +1,12 @@
-
 function search() {
-
-    // 在此处添加处理搜索请求的代码，例如向后端发送请求并展示搜索结果等。这里仅展示如何使用原生JavaScript进行搜索。
-    var input = document.getElementById('search-input'); // 获取输入框元素
-    var value = input.value; // 获取输入框中的值
-    console.log('Search term:', value); // 在控制台输出搜索词条
+ var input = document.getElementById("search-input");
+ var value = input.value.toLowerCase();
+ var result = document.getElementsByClassName("result");
+ for (var i = 0; i < result.length; i++) {
+  if (result[i].innerText.toLowerCase().indexOf(value) !== -1) {
+   result[i].style.display = "block";
+  } else {
+   result[i].style.display = "none";
+  }
+ }
 }
-
