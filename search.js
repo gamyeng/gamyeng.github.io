e97a400b8c7a23f99d63b7e0c770ee464567c5ce
gamyeng <133553103+gamyeng@users.noreply.github.com>
2023-05-17 21:12:06 +0800

    Create search.js

---
+++ search.js
@@ -0,0 +1,9 @@
+
+function search() {
+
+    // 在此处添加处理搜索请求的代码，例如向后端发送请求并展示搜索结果等。这里仅展示如何使用原生JavaScript进行搜索。
+    var input = document.getElementById('search-input'); // 获取输入框元素
+    var value = input.value; // 获取输入框中的值
+    console.log('Search term:', value); // 在控制台输出搜索词条
+}
+
